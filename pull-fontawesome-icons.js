bc9f8c6b97c388910a9dae1b285d973a64614763
Seth Clydesdale <sethc1995@gmail.com>
2018-02-24 11:11:50 -0500

    updates selectors for new fa-cheatsheet

--- pull-fontawesome-icons.js
+++ pull-fontawesome-icons.js
@@ -9,20 +9,21 @@
     type : 1,
     ignoreAlias : 1 // decide if you want to ignore the alias'
   },
-  a = $('.container > .row > div'), i = 0, j = a.length, t = document.createElement('TEXTAREA'), v = '';
+  a = document.querySelectorAll('.cheatsheet-set article span[class*="fa"]'), i = 0, j = a.length, t = document.createElement('TEXTAREA'), v = '';
   
   config.type ? v += 'var fa = {\n' : v += 'var fa = [';
   
   for (; i<j; i++) {
-    if (config.ignoreAlias && /\(alias\)/.test(a[i].innerHTML)) console.log('Skipping alias..');
-    else if (config.type) v += '  \'' + a[i].innerHTML.replace(/<.*?>.*?<\/.*?>|\s+/g, '').slice(3) + '\' : \'' + a[i].getElementsByTagName('I')[0].innerHTML + '\'' + ( i == j - 1 ? '' : ',' ) + '\n';
-    else if (!config.type) v += '\'' + a[i].getElementsByTagName('I')[0].innerHTML + '\'' + ( i == j - 1 ? '' : ',' );
+    if (config.ignoreAlias && /\(alias\)/.test(a[i].parentNode.nextElementSibling.querySelector('.word-wrap').innerHTML)) console.log('Skipping alias..');
+    else if (config.type) v += '  \'' + a[i].innerHTML + '\' : \'' + a[i].parentNode.nextElementSibling.querySelector('.word-wrap').innerHTML + '\'' + ( i == j - 1 ? '' : ',' ) + '\n';
+    else if (!config.type) v += '\'' + a[i].innerHTML + '\'' + ( i == j - 1 ? '' : ',' );
   }
   
   config.type ? v += '};' : v += '];';
   
   t.value = v;
-  t.setAttribute('style','position:fixed;left:0;bottom:0;z-index:9999;width:100%;height:200px;font-family:FontAwesome;');
+  t.className = 'fa fab fal far fas';
+  t.setAttribute('style','position:fixed;left:0;bottom:0;z-index:9999;width:100%;height:200px;font-family:"Font Awesome\ 5 Free", "Font Awesome\ 5 Brands", FontAwesome');
   t.onclick = function() { this.select() };
   document.body.appendChild(t);
 })();
